63f9147f45bfae4faa3ee2429068456b30b1617e
Vlad Morosanu <vladmorosanu97@gmail.com>
2021-01-24 20:44:32 +0200

    this is not a real project so next commit will be named:

--- client-project/marketplace-app/src/app/app.module.ts
+++ client-project/marketplace-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     RegisterModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
